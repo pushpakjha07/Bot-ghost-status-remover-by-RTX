db6c82c91f7c684fdf79612dd82b6089686af61e
pushpakjha07 <161748915+pushpakjha07@users.noreply.github.com>
2024-04-01 17:28:22 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["PLAYING","LITE X CHEATS"];
 
 
 let currentIndex = 0;
